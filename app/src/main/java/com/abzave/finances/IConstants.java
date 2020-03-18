797a8057e586eb2816e26366e72ff31d4f934b2c
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-03-18 15:33:47 -0600

    [New] Expenditures list

--- app/src/main/java/com/abzave/finances/IConstants.java
+++ app/src/main/java/com/abzave/finances/IConstants.java
@@ -1,5 +1,7 @@
 package com.abzave.finances;
 
+import android.database.sqlite.SQLiteDatabase;
+
 public interface IConstants {
 
     String ENTRY_STRING = "entry";
@@ -14,9 +16,17 @@ public interface IConstants {
     String INSERTION_FAILED_MESSAGE = "Falló la inserción";
     String COLONES = "Colones";
     String DOLLARS = "Dolares";
-    String CURRENCY_TYPE_QUERY = "select id from CurrencyType where type = ";
+    String CURRENCY_TYPE_QUERY = "SELECT id FROM CurrencyType WHERE type = ";
+    String EXPENDITURES_QUERY = "SELECT Expenditure.amount, Expenditure.description, " +
+                                "CurrencyType.type FROM Expenditure INNER JOIN CurrencyType on " +
+                                "Expenditure.currency = CurrencyType.id";
+    String NO_REGISTERS_MESSAGE = "No hay registros aún";
     int DATABASE_VERSION = 1;
     int ID_COLUMN = 0;
+    int DESCRIPTION_COLUMN = 1;
+    int AMOUNT_COLUMN = 0;
+    int CURRENCY_COLUMN = 2;
     boolean IS_ENTRY = true;
+    SQLiteDatabase.CursorFactory CURSOR_FACTORY = null;
 
 }
