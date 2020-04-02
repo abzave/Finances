0cef147b54b6abb24f465118eb8ddd4e0aca6b8a
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-04-01 18:28:32 -0600

    [Updated] Chart description

--- app/src/main/java/com/abzave/finances/activity/Chart.java
+++ app/src/main/java/com/abzave/finances/activity/Chart.java
@@ -5,12 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.abzave.finances.R;
+import com.abzave.finances.dataBase.IDataBaseConnection;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
 
-public class Chart extends AppCompatActivity {
+public class Chart extends AppCompatActivity implements IDataBaseConnection {
+
+    PieChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+        chart = findViewById(R.id.chart);
+        setUpChart();
     }
+
+    private void setUpChart(){
+        setChartDescription();
+    }
+
+    private void setChartDescription(){
+        Description description = new Description();
+        description.setText(ENTRIES_CHART_NAME);
+        chart.setDescription(description);
+    }
+
 }
--- app/src/main/java/com/abzave/finances/lib/IConstants.java
+++ app/src/main/java/com/abzave/finances/lib/IConstants.java
@@ -1,7 +1,5 @@
 package com.abzave.finances.lib;
 
-import android.database.sqlite.SQLiteDatabase;
-
 public interface IConstants {
 
     String ENTRY_STRING = "entry";
@@ -27,6 +25,7 @@ public interface IConstants {
     String RETIREMENT = "Retiro";
     String EMERGENCIES = "Emergencias";
     String WHIMS = "Caprichos";
+    String ENTRIES_CHART_NAME = "Ingresos";
     double RETIREMENT_PERCENTAGE = 0.05;
     double EMERGENCIES_PERCENTAGE = 0.1;
     double WHIMS_PERCENTAGE = 0.1;
