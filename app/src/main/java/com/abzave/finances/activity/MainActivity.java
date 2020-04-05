fc156692b047280ea1a9361b224b4dd49a6d8d81
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-04-05 17:54:31 -0600

    [Updated] Request external write permission

--- app/src/main/java/com/abzave/finances/activity/MainActivity.java
+++ app/src/main/java/com/abzave/finances/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.abzave.finances.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +19,11 @@ public class MainActivity extends AppCompatActivity implements IConstants {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_PERMISSION);
+        }
     }
 
     public void goToAddExpenditure(View view){
--- app/src/main/java/com/abzave/finances/lib/IConstants.java
+++ app/src/main/java/com/abzave/finances/lib/IConstants.java
@@ -23,6 +23,7 @@ public interface IConstants {
     float RETIREMENT_PERCENTAGE = 0.05f;
     float EMERGENCIES_PERCENTAGE = 0.1f;
     float WHIMS_PERCENTAGE = 0.1f;
+    int WRITE_EXTERNAL_STORAGE_PERMISSION = 1;
     short TOTAL_AMOUNT_CONTEXT = 0;
     short EXPENDED_CONTEXT = 1;
     short REMAINING_CONTEXT = 2;
