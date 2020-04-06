b10e58f91745066b8a2fc6daa3b3ffcde0324e8d
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-04-05 18:18:29 -0600

    [Updated] Backup Option

--- app/src/main/java/com/abzave/finances/activity/MainActivity.java
+++ app/src/main/java/com/abzave/finances/activity/MainActivity.java
@@ -8,7 +8,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.abzave.finances.R;
 import com.abzave.finances.lib.IConstants;
@@ -19,11 +22,29 @@ public class MainActivity extends AppCompatActivity implements IConstants {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions();
+    }
+
+    private void requestPermissions(){
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_PERMISSION);
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        createBackUp();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createBackUp(){
+
     }
 
     public void goToAddExpenditure(View view){
